127fe5ca5c7d55b4fecdae2585e7fdab03df4983
Naashw <hasan.taskin.pro@gmail.com>
2024-02-18 13:42:07 +0100

    wip

--- components/viewer.vue
+++ components/viewer.vue
@@ -1,15 +1,7 @@
 <template>
   <div class="full-size">
-    <View360
-      class="full-size"
-      ref="viewer"
-      :debug="true"
-      :projection="projection"
-      :on-view-change="onViewChange"
-      :on-ready="onReady"
-      :hotspot="{ zoom: true }"
-      :fov="80"
-    >
+    <View360 class="full-size" ref="viewer" :debug="true" :projection="projection" :on-view-change="onViewChange"
+      :on-ready="onReady" :hotspot="{ zoom: true }" :fov="80">
       <div class="view360-hotspots">
         <!-- Hotspots -->
         <div class="view360-hotspot" data-yaw="0" data-pitch="0">1</div>
@@ -32,6 +24,7 @@
 </template>
 <script setup lang="ts">
 import { View360, EquirectProjection, ViewChangeEvent } from "@egjs/vue3-view360";
+import { DefineComponent } from "nuxt/dist/app/compat/capi";
 
 interface VirtualTourDataItem {
   time: Date;
@@ -42,6 +35,8 @@ interface VirtualTourDataItem {
 }
 
 const virtualTourData = ref<VirtualTourDataItem[]>([]);
+const virtualTourAutomaticData = ref<VirtualTourDataItem[]>([]);
+const virtualTourAnimationIndex = ref(0);
 
 const projection = new EquirectProjection({
   src: "/360_mock.jpg",
@@ -49,48 +44,76 @@ const projection = new EquirectProjection({
 
 const viewer = ref(View360);
 
-  async function sendData() {
+async function sendData() {
   // Vérifiez s'il y a des données à envoyer
-  if(virtualTourData.value.length <= 0){
+  if (virtualTourData.value.length <= 0) {
     return;
   }
-    const dataSliced = virtualTourData.value.slice(); // Copiez les données à envoyer
-    virtualTourData.value = []; // Videz le tableau
+  const dataSliced = virtualTourData.value.slice(); // Copiez les données à envoyer
+  virtualTourData.value = []; // Videz le tableau
 
-    // Envoyez les données à l'API
-    try {
-      const response = await $fetch('virtualTour/save/', {
-        method: 'POST',
-        body: dataSliced,
-        baseURL: 'http://localhost:3001/',
-      });
+  // Envoyez les données à l'API
+  try {
+    const response = await $fetch('virtualTour/save/', {
+      method: 'POST',
+      body: dataSliced,
+      baseURL: 'http://localhost:3001/',
+    });
 
-      console.log('Données envoyées avec succès');
-    } catch (error) {
-      console.error('Erreur lors de l\'envoi des données :', error);
-    }
+    console.log('Données envoyées avec succès');
+  } catch (error) {
+    console.error('Erreur lors de l\'envoi des données :', error);
+  }
 }
 
-onMounted(() => {
-  viewer.value.view360.on("viewChange", onViewChange);
-   const sendingInterval = setInterval(sendData, 1000);
+async function fetchData() {
+  try {
+    const response: any = await $fetch('virtualTour/1/', {
+      method: 'GET',
+      baseURL: 'http://localhost:3001/',
+    });
 
-    onUnmounted(() => {
+    virtualTourAutomaticData.value = response;
+    startAnimationVirtualTour();
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error);
+  }
+}
+
+function startAnimationVirtualTour() {
+  console.log('lookAt');
+  console.log(viewer.value.view360.camera);
+  viewer.value.view360.camera.animateTo({yax: 270, pitch:270, zoom:1,duration: 2000});
+  // viewer.value.view360.zoom = 20;
+  // viewer.value.view360.lookAt('90', '90', '90', '1000');
+// setInterval(AnimateVirtualTour, 1000);
+}
+
+async function AnimateVirtualTour() {
+  const nextView: VirtualTourDataItem = virtualTourAutomaticData.value[virtualTourAnimationIndex.value]
+  return { yaw: nextView.yaw, pitch: nextView.pitch, fov: 90, delay: 1000 };
+  virtualTourAnimationIndex.value++;
+}
+
+onMounted(async () => {
+  viewer.value.view360.on("viewChange", onViewChange);
+  const sendingInterval = setInterval(sendData, 1000);
+  await fetchData();
+  onUnmounted(() => {
     clearInterval(sendingInterval);
   });
 
 });
 
 
-function  onViewChange(evt: ViewChangeEvent) {
-  console.log("evt", evt);
+function onViewChange(evt: ViewChangeEvent) {
   StockPosition(evt.yaw, evt.pitch, evt.zoom);
 };
 function onReady() {
   console.log("ready");
 };
 
-function StockPosition(yaw: number, pitch: number, zoom: number ): void {
+function StockPosition(yaw: number, pitch: number, zoom: number): void {
   virtualTourData.value.push({
     time: new Date(),
     virtualTourId: '1',
